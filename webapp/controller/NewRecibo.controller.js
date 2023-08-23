16e4440c56c02459b8db72ddc62620e3daeafac9
correamartinm <correamartinm@gmail.com>
2023-08-23 12:22:50 -0300

    camara inicio

--- webapp/controller/NewRecibo.controller.js
+++ webapp/controller/NewRecibo.controller.js
@@ -4,11 +4,15 @@ sap.ui.define(
     "sap/ui/model/FilterOperator",
     "sap/ui/model/Filter",
     "sap/ui/core/ValueState",
+    "sap/m/Dialog",
+    "sap/m/Button",
+    "../libs/Download",
+    "sap/m/MessageToast"
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (BaseController, FilterOperator, Filter, ValueState) {
+  function (BaseController, FilterOperator, Filter, ValueState, Dialog, Button, Download, MessageToast) {
     "use strict";
 
     return BaseController.extend("morixe.zfirecibos.controller.NewRecibo", {
@@ -312,11 +316,12 @@ sap.ui.define(
           oFile.setValueState(ValueState.None);
         }
 
-        oldData = this._onGetDataModel(oModel, oDescuentos);
-
-        let oValue = false,
-          oDescuentos = "/Descuentos";
+        
+        let oValue = false, oImportesSuma,
+        oDescuentos = "/Descuentos";
         this.onshowDescuentoAdd(oValue);
+        
+        oldData = this._onGetDataModel(oModel, oDescuentos);
 
         let oDatos = {
           Motivokey: oMotivo.getSelectedKey(),
@@ -331,7 +336,7 @@ sap.ui.define(
 
         for (var index = 0; index < DataFinal.length; index++) {
           oImportesSuma =
-            parseFloat(oImportesSuma) + parseFloat(oItems[index].Importe);
+            parseFloat(oImportesSuma) + parseFloat(DataFinal[index].Importe);
         }
 
         let oCantidad = "/Paso04CantidadDescuentos",
@@ -600,6 +605,83 @@ sap.ui.define(
       cancelarDetlles: function () {
         let oValue = false;
         this.onshowDetalleAdd(oValue);
+      },
+
+      // Photo
+
+      capturePic: function () {
+        var that = this;
+        this.cameraDialog = new Dialog({
+          title:  this._i18n().getText("dlgtitle"),
+          beginButton: new Button({
+            text:  this._i18n().getText("lblsacarfoto"),
+            press: function (oEvent) {
+              that.imageValue = document.getElementById("player");
+              var oButton = oEvent.getSource();
+              that.imageText = oButton.getParent().getContent()[1].getValue();
+              that.cameraDialog.close();
+            }
+          }),
+          content: [
+            new sap.ui.core.HTML({
+              content: "<video id='player' autoplay></video>"
+            }),
+            new sap.m.Input({
+              placeholder: "Please input image text here",
+              required: true
+            })
+          ],
+          endButton: new Button({
+            text: this._i18n().getText("btnvolver"),
+            press: function () {
+              that.cameraDialog.close();
+            }
+          })
+  
+        });
+        this.getView().addDependent(this.cameraDialog);
+        this.cameraDialog.open();
+        this.cameraDialog.attachBeforeClose(this.setImage, this);
+        if (navigator.mdeiaDevices) {
+          navigator.mediaDevices.getUserMedia({
+            video: true
+          }).then(function (stream) {
+            player.srcObject = stream;
+          });
+        }
+      },
+  
+      setImage: function () {
+        var oVBox = this.getView().byId("vBox1");
+        var oItems = oVBox.getItems();
+        var imageId = 'archie-' + oItems.length;
+        var fileName = this.imageText;
+        var imageValue = this.imageValue;
+        if (imageValue == null) {
+          MessageToast.show("No image captured");
+        } else {
+  
+          var oCanvas = new sap.ui.core.HTML({
+            content: "<canvas id='" + imageId + "' width='320px' height='320px' " +
+              " style='2px solid red'></canvas> "
+          });
+          var snapShotCanvas;
+  
+          oVBox.addItem(oCanvas);
+          oCanvas.addEventDelegate({
+            onAfterRendering: function () {
+              snapShotCanvas = document.getElementById(imageId);
+              var oContext = snapShotCanvas.getContext('2d');
+              oContext.drawImage(imageValue, 0, 0, snapShotCanvas.width, snapShotCanvas.height);
+              var imageData = snapShotCanvas.toDataURL('image/png');
+              var imageBase64 = imageData.substring(imageData.indexOf(",") + 1);
+              //	window.open(imageData);  --Use this if you dont want to use third party download.js file 
+              download(imageData, fileName + ".png", "image/png");
+  
+            }
+          });
+  
+        }
       },
 
       onButtonDeletePayPress: function (oEvent) {
